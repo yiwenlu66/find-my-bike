a5ec325e3e7824f524c5390704560dc26d4872c9
mcfloundinho <mcfloundinho@gmail.com>
2015-12-14 23:18:44 +0800

    Modularize

--- Arduino/bt/bt.ino
+++ Arduino/bt/bt.ino
@@ -7,7 +7,6 @@
 #define BAUDRATE 9600
 
 char junk;
-String inputString = "";
 
 int melody[] = {
     NOTE_E6, NOTE_C6, NOTE_D6, NOTE_A5,
@@ -49,6 +48,35 @@ int noteDurations[] = {
     4, 8, 8, 2
 };
 
+String input()
+{
+    String inputString = "";
+    while (Serial1.available()) {
+        char inChar = (char)Serial1.read();
+        inputString += inChar;
+    }
+    Serial1.println(inputString);
+    while (Serial1.available() > 0) {
+        junk = Serial1.read() ;
+    }
+    return inputString;
+}
+
+void play()
+{
+    for (int thisNote = 0; thisNote < sizeof(melody) / sizeof(int); thisNote++) {
+
+        int noteDuration = 1000 / noteDurations[thisNote];
+        tone(SOUNDPIN, melody[thisNote], noteDuration);
+
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+
+        noTone(SOUNDPIN);
+    }
+
+}
+
 void setup()
 {
     Serial1.begin(BAUDRATE);
@@ -58,30 +86,13 @@ void setup()
 void loop()
 {
     if (Serial1.available()) {
-        while (Serial1.available()) {
-            char inChar = (char)Serial1.read();
-            inputString += inChar;
-        }
-        Serial1.println(inputString);
-        while (Serial1.available() > 0) {
-            junk = Serial1.read() ;
-        }
-        if (inputString == ARG_LEDON) {
+        String inputString = input();
+
+        if (inputString == ARG_LEDON)
             digitalWrite(LEDPIN, HIGH);
-        } else if (inputString == ARG_LEDOFF) {
+        else if (inputString == ARG_LEDOFF)
             digitalWrite(LEDPIN, LOW);
-        } else if (inputString == ARG_SOUNDON) {
-            for (int thisNote = 0; thisNote < sizeof(melody) / sizeof(int); thisNote++) {
-
-                int noteDuration = 1000 / noteDurations[thisNote];
-                tone(SOUNDPIN, melody[thisNote], noteDuration);
-
-                int pauseBetweenNotes = noteDuration * 1.30;
-                delay(pauseBetweenNotes);
-
-                noTone(SOUNDPIN);
-            }
-        }
-        inputString = "";
+        else if (inputString == ARG_SOUNDON)
+            play();
     }
 }
